b2ea0a74b4c4d61fbdbaaa9b03b338bcec0a91f4
suryamin <suryamin@gmail.com>
2026-03-07 19:58:49 +1100

    7JAN2026

--- weather/ui/weatherDetailUI.tsx
+++ weather/ui/weatherDetailUI.tsx
@@ -43,7 +43,6 @@ export const WeatherDetailUI = () => {
   /* access to route.params.city   */
   /* ----------------------------- */
   const route = useRoute<WeatherDetailRouteProp>();
-  const test = "";
 
   /* ------------------------------ */
   /* access to navigation functions */
